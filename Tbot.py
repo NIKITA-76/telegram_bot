77088655173e011b435858b1f07bd31ba12f28e8
NIKITA-76 <73834676+NIKITA-76@users.noreply.github.com>
2022-01-11 05:15:39 +0300

    Added catching errors

--- Tbot.py
+++ Tbot.py
@@ -6,36 +6,42 @@ from telegram.ext import MessageHandler
 import requests
 
 
-class APIWeather():
+class APIWeather:
     def jsonOfAPI(self, city_name):
         link = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=metric&appid={"b41f154a86e9c4f96fd719a2d7207a11"}'
-        data_link_api = requests.get(link)
-        data_from_api = data_link_api.json()
+        link_api = requests.get(link)
+        data_from_api = link_api.json()
+        if data_from_api['cod'] == '404':
+            return "Error"
         return data_from_api
 
 
-
-
-class brainOfBot():
+class BrainOfBot:
 
     def message_handler(self, update: Update, context: CallbackContext):
         text = update.message.text
         data = APIWeather().jsonOfAPI(text)
-        update.message.reply_text(
+        if data != 'Error':
+            update.message.reply_text(
 
-            text=f"Сегодня в вашем городе {data['main']['temp']}℃\n"
-                 "\n"
-                 f"Максимальная температура {data['main']['temp_max']}℃\n"
-                 f"Минимальная температура {data['main']['temp_min']}℃\n"
-                 f"Ощущаестя как {data['main']['feels_like']}℃\n",
-        )
+                text=f"Сегодня в вашем городе {data['main']['temp']}℃\n"
+                     "\n"
+                     f"Максимальная температура {data['main']['temp_max']}℃\n"
+                     f"Минимальная температура {data['main']['temp_min']}℃\n"
+                     f"Ощущаестя как {data['main']['feels_like']}℃\n",
+            )
+        else:
+            update.message.reply_text(
+
+                text="Ошибка\n"
+                     "Вы некоректно ввели ваш город"
+            )
 
     def main(self):
         print("Start")
 
         updater = Updater(
             token='5093069989:AAFpImvrxEnqHc_oN9ElWIQX59ywnpYxaQw',
-            use_context=True,
         )
 
         updater.dispatcher.add_handler(MessageHandler(filters=Filters.all, callback=self.message_handler))
@@ -44,5 +50,6 @@ class brainOfBot():
 
 
 if __name__ == '__main__':
-    x = brainOfBot()
+    x = BrainOfBot()
     x.main()
+
